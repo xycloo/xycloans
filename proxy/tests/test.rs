e0268067e75ac700ced7a22e643f0b2c52be9f6b
heytdep <tommasodeponti@zohomail.eu>
2023-03-07 20:41:45 +0100

    fee withdrawal test

--- proxy/tests/test.rs
+++ proxy/tests/test.rs
@@ -268,3 +268,57 @@ fn proxy_invalid_admin_auth() {
     let _set_fl_res = proxy_client.try_set_fl(&not_protocol, &token_id, &flash_loan_contract_id);
     assert_eq!(e.recorded_top_authorizations(), []);
 }
+
+#[test]
+fn fee_withdrawal() {
+    let e: Env = Default::default();
+    let token_admin = Address::random(&e);
+    let protocol = Address::random(&e);
+    let lp = Address::random(&e);
+
+    let token_id = e.register_stellar_asset_contract(token_admin.clone());
+    let token = token::Client::new(&e, &token_id);
+    token.mint(&token_admin, &lp, &40000000000);
+
+    let proxy_contract_id =
+        e.register_contract_wasm(&BytesN::from_array(&e, &[1; 32]), proxy::WASM);
+    let proxy_client = proxy::Client::new(&e, &proxy_contract_id);
+    let proxy_id = Address::from_contract_id(&e, &proxy_contract_id);
+
+    let vault_contract_id =
+        e.register_contract_wasm(&BytesN::from_array(&e, &[8; 32]), vault::WASM);
+    let vault_client = vault::Client::new(&e, &vault_contract_id);
+    let vault_id = Address::from_contract_id(&e, &vault_contract_id);
+
+    let flash_loan_contract_id =
+        e.register_contract_wasm(&BytesN::from_array(&e, &[5; 32]), loan_ctr::WASM);
+    let flash_loan_client = loan_ctr::Client::new(&e, &flash_loan_contract_id);
+    let flash_loan_id = Address::from_contract_id(&e, &flash_loan_contract_id);
+
+    proxy_client.initialize(&protocol);
+
+    flash_loan_client.init(&token_id, &vault_id);
+    vault_client.initialize(
+        &proxy_id,
+        &token_id,
+        &flash_loan_id,
+        &flash_loan_contract_id,
+    );
+
+    proxy_client.set_vault(&protocol, &token_id, &vault_contract_id);
+    proxy_client.set_fl(&protocol, &token_id, &flash_loan_contract_id);
+
+    proxy_client.deposit(&lp, &token_id, &40000000000);
+
+    assert_eq!(token.balance(&lp), 0);
+    assert_eq!(token.balance(&flash_loan_id), 40000000000);
+    assert_eq!(token.balance(&vault_id), 0);
+
+    let batch_0 = vault_client.get_shares(&lp, &0).unwrap();
+
+    proxy_client.fee_width(&lp, &token_id, &0, &300000000);
+
+    let updated_batch_0 = vault_client.get_shares(&lp, &0).unwrap();
+
+    assert_eq!(updated_batch_0.curr_s, batch_0.curr_s - 300000000);
+}
