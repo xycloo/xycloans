be5d07073290f8eceeb97f7295cb04d3fa2b99f8
heytdep <tommasodeponti@zohomail.eu>
2023-04-13 21:49:27 +0200

    successful auth test completed

--- vault/tests/auth.rs
+++ vault/tests/auth.rs
@@ -16,8 +16,17 @@ mod loan_ctr {
     contractimport!(file = "../target/wasm32-unknown-unknown/release/xycloans_flash_loan.wasm");
 }
 
+mod receiver_interface {
+    use soroban_sdk::contractimport;
+
+    contractimport!(
+        file = "../target/wasm32-unknown-unknown/release/soroban_flash_loan_receiver_standard.wasm"
+    );
+}
+use crate::flash_loan_receiver_standard::FlashLoanReceiverClient;
+
+use soroban_sdk::{contractimpl, vec, IntoVal, RawVal, Symbol};
 use soroban_sdk::{testutils::Address as _, Address, BytesN, Env};
-use soroban_sdk::{vec, IntoVal, RawVal, Symbol};
 
 #[test]
 fn vault_admin_auth() {
@@ -36,9 +45,23 @@ fn vault_admin_auth() {
     let vault_id = Address::from_contract_id(&e, &vault_contract_id);
 
     let flash_loan_contract_id =
-        e.register_contract_wasm(&BytesN::from_array(&e, &[8; 32]), loan_ctr::WASM);
-    let flash_loan_id = Address::from_contract_id(&e, &flash_loan_contract_id);
+        e.register_contract_wasm(&BytesN::from_array(&e, &[23; 32]), loan_ctr::WASM);
     let flash_loan_client = loan_ctr::Client::new(&e, &flash_loan_contract_id);
+    let flash_loan_id = Address::from_contract_id(&e, &flash_loan_contract_id);
+
+    let increment_contract = e.register_contract(&BytesN::from_array(&e, &[2; 32]), BalIncrement);
+    let increment_contract_id = Address::from_contract_id(&e, &increment_contract);
+    let increment_client = BalIncrementClient::new(&e, &increment_contract);
+
+    let receiver_contract =
+        e.register_contract(None, crate::flash_loan_receiver_standard::FlashLoanReceiver);
+    let receiver_contract_id = Address::from_contract_id(&e, &receiver_contract);
+    let receiver_client = FlashLoanReceiverClient::new(&e, &receiver_contract);
+
+    token.mint(&increment_contract_id, &1000000000);
+
+    receiver_client.init(&user1, &token_id);
+    increment_client.init(&user1, &token_id);
 
     flash_loan_client.init(&token_id, &vault_id);
     vault_client.initialize(&user1, &token_id, &flash_loan_id, &flash_loan_contract_id);
@@ -60,27 +83,26 @@ fn vault_admin_auth() {
     )];
     assert_eq!(e.recorded_top_authorizations(), expected_auth);
 
-    vault_client.withdraw_fee(&user1, &user1, &0, &(5 * STROOP as i128));
+    vault_client.withdraw_matured(&user1, &user1);
     let expected_auth: Vec<(Address, BytesN<32>, Symbol, soroban_sdk::Vec<RawVal>)> = std::vec![(
         user1.clone(),
         vault_contract_id.clone(),
-        Symbol::new(&e, "withdraw_fee"),
-        vec![
-            &e,
-            user1.into_val(&e),
-            user1.into_val(&e),
-            0_i128.into_val(&e),
-            (5 * STROOP as i128).into_val(&e),
-        ],
+        Symbol::new(&e, "withdraw_matured"),
+        vec![&e, user1.into_val(&e), user1.into_val(&e),],
     )];
     assert_eq!(e.recorded_top_authorizations(), expected_auth);
 
-    vault_client.withdraw(&user1, &user1);
+    vault_client.withdraw(&user1, &user1, &0);
     let expected_auth: Vec<(Address, BytesN<32>, Symbol, soroban_sdk::Vec<RawVal>)> = std::vec![(
         user1.clone(),
         vault_contract_id,
         Symbol::short("withdraw"),
-        vec![&e, user1.into_val(&e), user1.into_val(&e),],
+        vec![
+            &e,
+            user1.into_val(&e),
+            user1.into_val(&e),
+            (0_i128).into_val(&e)
+        ],
     )];
     assert_eq!(e.recorded_top_authorizations(), expected_auth);
 }
@@ -117,11 +139,102 @@ fn vault_admin_invalid_auth() {
     let _res = vault_client.try_deposit(&not_user1, &user1, &(5 * STROOP as i128));
     assert_eq!(e.recorded_top_authorizations(), []);
 
-    let _res = vault_client.try_withdraw_fee(&not_user1, &user1, &0, &(5 * STROOP as i128));
+    let _res = vault_client.try_withdraw_matured(&not_user1, &user1);
     assert_eq!(e.recorded_top_authorizations(), []);
 
-    let _res = vault_client.try_withdraw(&not_user1, &user1);
+    let _res = vault_client.try_withdraw(&not_user1, &user1, &0);
     assert_eq!(e.recorded_top_authorizations(), []);
 }
 
 use fixed_point_math::STROOP;
+
+mod flash_loan_receiver_standard {
+    use super::BalIncrementClient;
+    use crate::{receiver_interface, token};
+    use fixed_point_math::STROOP;
+    use soroban_sdk::{contractimpl, Address, BytesN, Env, Symbol};
+
+    pub struct FlashLoanReceiver;
+
+    fn compute_fee(amount: &i128) -> i128 {
+        amount / 2000 // 0.05%, still TBD
+    }
+
+    #[contractimpl]
+    impl FlashLoanReceiver {
+        pub fn init(e: Env, admin: Address, token: BytesN<32>) {
+            admin.require_auth();
+            e.storage().set(&Symbol::short("T"), &token);
+        }
+
+        pub fn exec_op(e: Env) -> Result<(), receiver_interface::ReceiverError> {
+            let token_client = token::Client::new(
+                &e,
+                &e.storage()
+                    .get::<Symbol, BytesN<32>>(&Symbol::short("T"))
+                    .unwrap()
+                    .unwrap(),
+            );
+            let client = BalIncrementClient::new(&e, &BytesN::from_array(&e, &[2; 32]));
+
+            token_client.transfer(
+                &e.current_contract_address(),
+                &Address::from_contract_id(&e, &BytesN::from_array(&e, &[2; 32])),
+                &(100 * STROOP as i128),
+            );
+            client.increment(&e.current_contract_address(), &(100 * STROOP as i128));
+
+            let total_amount = (100 * STROOP as i128) + compute_fee(&(100 * STROOP as i128));
+
+            token_client.increase_allowance(
+                &e.current_contract_address(),
+                &Address::from_contract_id(&e, &BytesN::from_array(&e, &[23; 32])),
+                &total_amount,
+            );
+
+            Ok(())
+        }
+    }
+}
+
+pub struct BalIncrement;
+
+#[contractimpl]
+impl BalIncrement {
+    pub fn init(e: Env, admin: Address, token: BytesN<32>) {
+        admin.require_auth();
+        e.storage().set(&Symbol::short("T"), &token);
+    }
+
+    pub fn increment(e: Env, id: Address, amount: i128) {
+        let token_client = token::Client::new(
+            &e,
+            &e.storage()
+                .get::<Symbol, BytesN<32>>(&Symbol::short("T"))
+                .unwrap()
+                .unwrap(),
+        );
+
+        token_client.transfer(
+            &e.current_contract_address(),
+            &id,
+            &(amount + STROOP as i128),
+        )
+    }
+
+    pub fn decrement(e: Env, id: Address, amount: i128) {
+        let token_client = token::Client::new(
+            &e,
+            &e.storage()
+                .get::<Symbol, BytesN<32>>(&Symbol::short("T"))
+                .unwrap()
+                .unwrap(),
+        );
+
+        token_client.transfer(
+            &e.current_contract_address(),
+            &id,
+            &(amount - STROOP as i128),
+        )
+    }
+}
