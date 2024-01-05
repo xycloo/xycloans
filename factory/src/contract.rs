5507adcefc6f00ffe25d78720a47ae3af1263b82
heytdep <tommasodeponti@zohomail.eu>
2024-01-05 19:16:58 +0100

    patchup

--- factory/src/contract.rs
+++ factory/src/contract.rs
@@ -12,7 +12,7 @@ pub trait PluggableInterface {
     /// Plugs in the protocol a vault contract for a certain token.
     /// Once both the vault and the associated flash loan are plugged in the proxy, there effictively is a new pool in the protocol.
     ///
-    /// [`set_vault()`] must be provided with:    /// [`set_vault()`] must be provided with:
+    /// [`set_pool()`] must be provided with:
     /// [`token_address: Address`] Address of the token used by the vault.
     /// [`pool_address: Address`] Address of the vault contract.
     fn set_pool(env: Env, token_address: Address, pool_address: Address) -> Result<(), Error>;
@@ -23,17 +23,18 @@ pub trait AdminInterface {
 
     /// [`initialize()`] must be provided with:
     /// [`admin: Address`] Address of the proxy's admin
+    /// [`pool_hash: BytesN<32>`] Hash of the pool
 
     /// The proxy's admin will only be able to plug in and out pools from the protocol
     /// without having any control over the deposited funds.
     fn initialize(env: Env, admin: Address, pool_hash: BytesN<32>) -> Result<(), Error>;
 
-    /// Deploys a flash loan-vault pair and initializes them accordingly.
+    /// Deploys a pool.
     fn deploy_pool(env: Env, token_address: Address, salt: BytesN<32>) -> Result<Address, Error>;
 }
 
 pub trait Common {
-    /// Reads from the storage the flash loan contract for a given token
+    /// Reads from the storage the pool contract for a given token
     fn get_pool_address(env: Env, token_address: Address) -> Result<Address, Error>;
 }
 
@@ -85,7 +86,7 @@ impl PluggableInterface for XycloansFactory {
     fn set_pool(env: Env, token_address: Address, pool_address: Address) -> Result<(), Error> {
         read_admin(&env)?.require_auth();
 
-        set_vault(&env, token_address, vault_address);
+        set_pool(&env, token_address, &pool_address);
         Ok(())
     }
 }
