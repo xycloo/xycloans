d41c51c1296ed1f3d7223f4eedca14f31e83c20d
heytdep <tommaso@xycloo.com>
2024-12-09 19:10:01 +0100

    add iface

---
+++ moderc3156/src/lib.rs
@@ -0,0 +1,6 @@
+use soroban_sdk::{contractclient, Address, Env};
+
+#[contractclient(name = "FlashLoanClient")]
+pub trait ModErc3156 {
+    fn exec_op(env: Env, caller: Address, token: Address, amount: i128, fee: i128);
+}
